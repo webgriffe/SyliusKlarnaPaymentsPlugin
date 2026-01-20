500133cc95ffdbd6d159be14e7da165d5a689cea
Lorenzo Ruozzi <lruozzi@webgriffe.com>
2026-01-20 11:47:11 +0100

    Fix Behat tests

--- tests/TestApplication/config/services_test.php
+++ tests/TestApplication/config/services_test.php
@@ -2,11 +2,21 @@
 
 declare(strict_types=1);
 
+namespace Symfony\Component\DependencyInjection\Loader\Configurator;
+
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
+use Tests\Webgriffe\SyliusKlarnaPaymentsPlugin\Service\DummyClient;
 
 return static function (ContainerConfigurator $container) {
     if (str_starts_with($container->env(), 'test')) {
         $container->import('../../../vendor/sylius/sylius/src/Sylius/Behat/Resources/config/services.xml');
         $container->import('@WebgriffeSyliusKlarnaPaymentsPlugin/tests/Behat/Resources/services.php');
+        $services = $container->services();
+
+        $services->set('webgriffe_sylius_klarna_payments.client', DummyClient::class)
+            ->args([
+                service('router'),
+            ])
+        ;
     }
 };
